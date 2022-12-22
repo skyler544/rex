2ca6c5244d049e1e71ec5863997d5b1cc73a9229
Skyler Mayfield <skyler544@gmail.com>
2022-12-22 15:03:14 +0100

    fix warning type

--- config/rex-settings.el
+++ config/rex-settings.el
@@ -1,13 +1,16 @@
 (use-package emacs
   :config
   ;; disable some automatic warnings
-  (add-to-list 'warning-suppress-types '((comp)))
-  (add-to-list 'warning-suppress-log-types '((comp)))
+  (add-to-list 'warning-suppress-types '(comp))
+  (add-to-list 'warning-suppress-log-types '(comp))
 
   ;; keep emacs directory cleaner
   (setq savehist-file (concat user-emacs-directory ".cache/minibuffer-history"))
   (setq transient-history-file (concat user-emacs-directory ".cache/transient-history"))
   (setq tramp-auto-save-directory (concat user-emacs-directory ".cache/tramp-autosave"))  
+
+  ;; do not add custom settings to the end of init.el
+  (setq custom-file "/dev/null")
   
   ;; general settings
   (column-number-mode)
