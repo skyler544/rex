f724a82cc543ac30b2f0bde305abb2245ee804f7
Skyler Mayfield <skyler544@gmail.com>
2022-12-22 15:41:36 +0100

    add recentf configuration

--- config/rex-settings.el
+++ config/rex-settings.el
@@ -5,6 +5,14 @@
   (add-to-list 'warning-suppress-types '(comp))
   (add-to-list 'warning-suppress-log-types '(comp)))
 
+(use-package recentf
+  :config
+  (setq recentf-save-file (concat user-emacs-directory ".cache/recentf"))
+  (setq recentf-max-saved-items 200)
+  (setq recentf-max-menu-items 200)
+  (recentf-mode 1)
+  (run-at-time nil (* 5 60) 'recentf-save-list))
+
 (use-package emacs
   :config
   ;; keep emacs directory cleaner
