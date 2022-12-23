5ced81c135867379e1ee439c0ad62f43f5ab342f
Skyler Mayfield <skyler544@gmail.com>
2022-12-23 16:40:18 +0100

    begin documenting the code

--- config/rex-bootstrap.el
+++ config/rex-bootstrap.el
@@ -1,12 +1,23 @@
+;; This file initializes the package system and installs use-package.
+;;
+;; This file should not be modified frequently (if at all).
+
+;; See this page for more information about the choices behind this
+;; initialization method.
 ;; https://stackoverflow.com/questions/31079204/emacs-package-install-script-in-init-file
 
+;; This setting must be made before (require 'use-package)
 (setq use-package-enable-imenu-support t)
 
+;; Add the melpa package archive and initialize the
+;; package system.
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
-
 (package-initialize)
 
+;; Creating a list of packages in this way allows the code
+;; to be more reusable, though this is not actually intended
+;; to be used for installing anything other than use-package.
 (setq rex-packages
       '(use-package))
 
@@ -16,8 +27,7 @@
   (when (and (not (package-installed-p pkg)) (assoc pkg package-archive-contents))
     (package-install pkg)))
 
-(require 'use-package)
-
+;; After this point, all further configuration should be made via use-package.
 (use-package use-package
   :init
   (setq use-package-always-ensure t))
--- init.el
+++ init.el
@@ -1,7 +1,24 @@
+;; Welcome to rex! This Emacs configuration is intended to be simple
+;; to understand and extend. Emphasis is placed on the use of well-
+;; documented packages. Configuration is aided and organized via
+;; use-package.
+;;
+;; Each use-package block should contain configuration for a single
+;; package and should be entirely standalone. Disabling a use-package
+;; block should not break the operation of any other package.
+;;
+;; Configuration for specific areas-of-concern should be done
+;; within a standalone file inside the config/ directory. Such
+;; files may then be explicitly loaded here. Disabling a file
+;; by removing or commenting out the line that loads it should
+;; not break the rest of the configuration. One exception is the
+;; bootstrap file; without it, use-package will not be loaded
+;; and none of the rest of the configuration will work.
+
 (add-to-list 'load-path (concat user-emacs-directory "config/"))
 
 ;; do not add custom settings to the end of init.el
-(setq custom-file "/dev/null")
+(setq custom-file ".cache/custom.el")
 
 (load "rex-bootstrap")
 (load "rex-settings")
