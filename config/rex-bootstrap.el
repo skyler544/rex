8504b10f01683c5d181de95074f81e464c3427ac
Skyler Mayfield <skyler544@gmail.com>
2022-12-23 11:33:03 +0100

    add imenu support to use-package

--- config/rex-bootstrap.el
+++ config/rex-bootstrap.el
@@ -1,5 +1,7 @@
 ;; https://stackoverflow.com/questions/31079204/emacs-package-install-script-in-init-file
 
+(setq use-package-enable-imenu-support t)
+
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
 
@@ -13,6 +15,8 @@
 (dolist (pkg rex-packages)
   (when (and (not (package-installed-p pkg)) (assoc pkg package-archive-contents))
     (package-install pkg)))
+
+(require 'use-package)
 
 (use-package use-package
   :init
