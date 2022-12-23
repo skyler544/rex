cf1de214caed55f868216aca7b4a3e53c0744458
Skyler Mayfield <skyler544@gmail.com>
2022-12-23 17:27:57 +0100

    add crux bindings

--- config/rex-qol.el
+++ config/rex-qol.el
@@ -3,3 +3,13 @@
 
 (use-package avy
   :bind ("C-;" . avy-goto-char-timer))
+
+(use-package crux
+  :bind (("S-<return>" . crux-smart-open-line)
+	 ("C-S-<return>" . crux-smart-open-line-above)
+	 ("C-c n" . crux-cleanup-buffer-or-region)
+	 ("C-c r" . crux-rename-file-and-buffer)
+	 ("C-j" . crux-top-join-line)
+	 ("C-k" . crux-kill-whole-line)
+	 ("C-S-k" . kill-line)
+	 ("C-c d" . crux-duplicate-current-line-or-region)))
