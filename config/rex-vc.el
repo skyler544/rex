bb320a2892180bd59e799db17730627069c22d80
Skyler Mayfield <skyler544@gmail.com>
2022-12-23 17:30:48 +0100

    fix magit keybindings

--- config/rex-vc.el
+++ config/rex-vc.el
@@ -1,5 +1,8 @@
 (use-package magit
-  :defer t)
+  :defer t
+  :bind (:map magit-mode-map
+	      ("x" . magit-delete-thing)
+	      ("k" . magit-reset-quickly)))
 
 (use-package git-timemachine
   :defer t)
