41b143c35d22190a9ab1d1148469b183de530b0b
Skyler Mayfield <skyler544@gmail.com>
2022-12-25 11:31:31 +0100

    evil collection already defines these keys

--- config/rex-vc.el
+++ config/rex-vc.el
@@ -1,10 +1,4 @@
-(use-package magit
-  :bind
-  (:map magit-mode-map
-	("x" . magit-delete-thing))
-  (:map magit-status-mode-map
-	("k" . previous-line)
-	("j" . next-line)))
+(use-package magit)
 
 (use-package git-timemachine
   :commands (git-timemachine))
