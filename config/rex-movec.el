7f078e0df0c80c05b9bcf6def77e931cde00d3b0
Skyler Mayfield <skyler544@gmail.com>
2022-12-25 20:06:58 +0100

    document movec

--- config/rex-movec.el
+++ config/rex-movec.el
@@ -1,3 +1,5 @@
+;; This provides a vertical view of the current candidates while
+;; interacting with the minibuffer.
 (use-package vertico
   :init
   (vertico-mode)
@@ -9,6 +11,8 @@
   (setq vertico-count 12)
   (setq vertico-resize nil))
 
+;; This extends vertico with subjectively nicer keybindings for
+;; navigating directories.
 (use-package vertico-directory
   :after vertico
   :ensure nil
@@ -19,16 +23,28 @@
             "M-DEL" 'vertico-directory-delete-word)
   :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
 
+;; This completion style makes "narrowing" from a list of candidates
+;; very quick and intuitive. It also provides nice font-faces for
+;; visually distinguishing what exactly you're matching on in a
+;; string.
 (use-package orderless
   :init
   (setq completion-styles '(orderless basic))
   (setq completion-category-defaults nil)
   (setq completion-category-overrides '((file) (styles partial-completion))))
 
+;; This package adds information to the minibuffer that can be used in
+;; various ways. The information can be very nice by itself, but it
+;; also adds context to certain minibuffer actions which are lacking
+;; by default, automatically making the matches more relevant. This is
+;; directly used by Embark.
 (use-package marginalia
   :init
   (marginalia-mode))
 
+;; A context-aware keyboard-driven equivalent of a right-click
+;; menu. This thing is a swiss-army-knife on steroids. Beware; it will
+;; make everything better than it was before.
 (use-package embark
   :general
   ("C-," 'embark-act)
@@ -39,6 +55,10 @@
   (setq embark-indicators '(embark-minimal-indicator))
   (setq embark-prompter 'embark-completing-read-prompter))
 
+;; A very smart package that provides lots of nice functions. Many of
+;; them are helpful for searching, but there are also commands to help
+;; with jumping between various things and interacting with the kill
+;; ring. Combine with Embark and mix well.
 (use-package consult
   :general
   ("P" 'consult-yank-pop)
@@ -61,7 +81,10 @@
   (rex-local-leader
     "m" 'consult-mode-command))
 
+;; Allows you to insert directories into the minibuffer prompt. It
+;; integrates with the minibuffer history, so it gets smarter the more
+;; you use it.
 (use-package consult-dir
   :general
   (:keymaps 'vertico-map
-            "C-x C-d" 'consult-dir))
+            "M-d" 'consult-dir))
