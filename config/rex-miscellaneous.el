5a531056086457c0af9617ef355935b605675837
Skyler Mayfield <skyler544@gmail.com>
2022-12-27 20:16:06 +0100

    document new file

--- config/rex-miscellaneous.el
+++ config/rex-miscellaneous.el
@@ -1,3 +1,6 @@
+;;; -*- lexical-binding: t -*-
+;;
+;; This file holds test code that may or may not work or come in handy someday.
 (use-package emacs
   :config
   (defun rex/set-capf-nonexclusive (local-capf)
