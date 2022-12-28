c8ecff09551239929728c5d3c8ec29f62d167500
Skyler Mayfield <skyler544@gmail.com>
2022-12-28 14:46:25 +0100

    keybindigns for eglot

--- config/rex-programming.el
+++ config/rex-programming.el
@@ -2,10 +2,17 @@
 ;;
 ;; Add support for some programming tools such as language servers.
 (use-package eglot
+  :general
+  (rex-leader
+    "cf" 'eglot-format-buffer
+    "cr" 'eglot-rename
+    "ca" 'eglot-code-actions)
   :commands (eglot))
 
 (use-package eglot-java
-  :hook (java-mode . eglot-java-mode))
+  :hook
+  (java-mode . eglot-java-mode)
+  (java-mode . (lambda () (setq tab-width 4))))
 
 (use-package lua-mode
   :config
