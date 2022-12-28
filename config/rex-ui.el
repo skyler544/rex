b267d8bd4bad99414e48540cb6b4935e211ca9d7
Skyler Mayfield <skyler544@gmail.com>
2022-12-28 13:48:58 +0100

    kind icons is broken for some reason; remove it

--- config/rex-ui.el
+++ config/rex-ui.el
@@ -31,13 +31,6 @@
   (load-theme 'creamsody t)
   (creamsody-modeline))
 
-(use-package kind-icon
-  :after corfu
-  :custom
-  (kind-icon-default-face 'corfu-default)
-  :config
-  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
-
 (use-package all-the-icons)
 
 (use-package all-the-icons-dired
