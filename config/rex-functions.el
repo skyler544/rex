8765a38cfe44f2bf6e31fa4e0f644231bc77e60b
Skyler Mayfield <skyler544@gmail.com>
2022-12-30 19:09:35 +0100

    add functions for moving to the newly opened window

--- config/rex-functions.el
+++ config/rex-functions.el
@@ -6,7 +6,21 @@
   :config
   (defun rex/scroll-other-window-down ()
     (interactive)
-      (scroll-other-window 1))
+    (scroll-other-window 1))
   (defun rex/scroll-other-window-up ()
     (interactive)
-      (scroll-other-window -1)))
+    (scroll-other-window -1))
+  (defun rex/split-and-follow-horizontally ()
+    "Split window below."
+    (interactive)
+    (split-window-below)
+    (other-window 1))
+  (defun rex/split-and-follow-vertically ()
+    "Split window right."
+    (interactive)
+    (split-window-right)
+    (other-window 1))
+  :general
+  (rex-leader
+    "ws" 'rex/split-and-follow-horizontally
+    "wv" 'rex/split-and-follow-vertically))
