ec71974055d6ebc7c964bcf71c49241da1fc8fa2
Skyler Mayfield <skyler544@gmail.com>
2022-12-30 17:12:56 +0100

    add more pulsar functions

--- config/rex-ui.el
+++ config/rex-ui.el
@@ -61,61 +61,20 @@
   :init
   (setq pulsar-face 'pulsar-generic)
   :config
-
   (setq pulsar-functions
         '(evil-window-down
           evil-window-up
           evil-window-right
           evil-window-left
-          avy-goto-char-timer))
+          evil-avy-goto-char-timer
+          isearch-repeat-forward
+          isearch-repeat-backward))
   (dolist (fkt pulsar-functions)
     (add-to-list 'pulsar-pulse-functions fkt))
-
   (face-spec-reset-face 'pulsar-generic)
   (set-face-attribute 'pulsar-generic nil :inherit 'region)
-
   (pulsar-global-mode))
 
 ;; Highlight hex color strings (and some other kinds) in the buffer
 (use-package rainbow-mode
   :commands 'rainbow-mode)
-
-;; Redefine the fringe arrows.
-(use-package emacs
-  :config
-  (define-fringe-bitmap 'right-arrow
-    [#b00001110
-     #b00001110
-     #b00001110
-     #b11111110
-     #b11111110
-     #b00001110
-     #b00001110
-     #b00001110])
-  (define-fringe-bitmap 'left-arrow
-    [#b01110000
-     #b01110000
-     #b01110000
-     #b01111111
-     #b01111111
-     #b01110000
-     #b01110000
-     #b01110000])
-  (define-fringe-bitmap 'right-curly-arrow
-    [#b00000000
-     #b00000000
-     #b01111100
-     #b01111100
-     #b00001100
-     #b00001100
-     #b00001100
-     #b00001100])
-  (define-fringe-bitmap 'left-curly-arrow
-    [#b00110000
-     #b00110000
-     #b00110000
-     #b00110000
-     #b00111110
-     #b00111110
-     #b00000000
-     #b00000000]))
