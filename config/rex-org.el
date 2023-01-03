3a3d940e344400e09b7d73ab1ef3c3a00dd4ded6
Skyler Mayfield <skyler544@gmail.com>
2023-01-03 14:12:32 +0100

    move the dwim function to the main org config now that it's shorter

--- config/rex-org.el
+++ config/rex-org.el
@@ -1,10 +1,8 @@
 ;;; -*- lexical-binding: t -*-
 ;;
 ;; General settings
-(load "rex-org-functions")
 (use-package org
   :config
-  (setq-default org-agenda-window-setup 'current-window)
   (setq org-link-frame-setup '((file . find-file)))
   (setq org-fontify-whole-heading-line t)
   (setq org-startup-indented t)
@@ -21,11 +19,16 @@
 ;; Agenda / workflow settings
 (use-package org
   :config
+  (setq-default org-agenda-window-setup 'current-window)
+  (setq org-agenda-span 10)
+  (setq org-agenda-start-on-weekday nil)
+  (setq org-agenda-start-day "-3d")
+  (setq org-agenda-inhibit-startup t)
+  (setq org-agenda-files '("~/mega/org/todo.org" "~/mega/org/fh.org" "~/mega/org/habits.org"))
   (setq org-log-into-drawer t)
   (setq org-log-done 'time)
   (setq org-enforce-todo-dependencies t)
-  (setq org-todo-keywords '((sequence "TODO" "PROJ" "IDEA" "|" "DONE")))
-  (setq org-agenda-files '("~/mega/org/todo.org" "~/mega/org/fh.org" "~/mega/org/habits.org")))
+  (setq org-todo-keywords '((sequence "TODO" "PROJ" "IDEA" "|" "DONE"))))
 
 ;; Font settings
 (use-package org
@@ -43,11 +46,43 @@
   (dolist (face '(org-block org-block-begin-line org-block-end-line))
     (set-face-attribute face nil :background nil)))
 
+(use-package org-contrib
+  :config
+  (setq org-eldoc-breadcrumb-separator " :: "))
+
 (use-package evil-org
   :config (defun rex/start-evil-org-mode ()
             (evil-org-mode))
   :after org
   :hook (org-mode . rex/start-evil-org-mode))
+
+(use-package org
+  :config
+  ;; A function that is mostly just stolen from Doom, but is trimmed down to only
+  ;; do things that I need it to do.
+  (defun rex/org-dwim-at-point (&optional arg)
+    "Do what I mean at point. This will toggle the todo state of a
+headline or follow a link."
+    (interactive)
+    (let* ((context (org-element-context))
+           (type (org-element-type context)))
+      (pcase type
+        ;; toggle todo state
+        (`headline
+         (cond ((or (org-element-property :todo-type context)
+                    (org-element-property :scheduled context))
+                (org-todo
+                 (if (eq (org-element-property :todo-type context) 'done)
+                     'todo
+                   'done))))
+         (org-update-checkbox-count)
+         (org-update-parent-todo-statistics))
+        ;; follow a link
+        (`link (org-open-at-point))
+        ;; toggle a checkbox
+        ((guard (org-element-property :checkbox (org-element-lineage context '(item) t)))
+         (let ((match (and (org-at-item-checkbox-p) (match-string 1))))
+           (org-toggle-checkbox (if (equal match "[ ]") '(16)))))))))
 
 ;; keybindings
 (use-package org
