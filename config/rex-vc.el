fc3163f4e123f556d62e0929252f7694ee9fb772
Skyler Mayfield <skyler544@gmail.com>
2023-01-20 20:59:01 +0100

    adds a config that makes magit-status take over the whole frame
    I'm not actually sure if I like this or not...

--- config/rex-vc.el
+++ config/rex-vc.el
@@ -5,9 +5,29 @@
   :config
   (setq magit-diff-refine-hunk t)
   (setq magit-save-repository-buffers nil)
+  (defun rex/magit-status ()
+    "Open a `magit-status' buffer and close the other window so only Magit is visible.
+If a file was visited in the buffer that was active when this
+command was called, go to its unstaged changes section."
+    (interactive)
+    (let* ((buffer-file-path (when buffer-file-name
+                               (file-relative-name buffer-file-name
+                                                   (locate-dominating-file buffer-file-name ".git"))))
+           (section-ident `((file . ,buffer-file-path) (unstaged) (status))))
+      (call-interactively #'magit-status)
+      (delete-other-windows)
+      (when buffer-file-path
+        (goto-char (point-min))
+        (cl-loop until (when (equal section-ident (magit-section-ident (magit-current-section)))
+                         (magit-section-show (magit-current-section))
+                         (recenter)
+                         t)
+                 do (condition-case nil
+                        (magit-section-forward)
+                      (error (cl-return (magit-status-goto-initial-section-1))))))))
   :general
   (rex-leader
-    "gg" 'magit-status))
+    "gg" 'rex/magit-status))
 
 ;; Show previous versions of a file
 (use-package git-timemachine
