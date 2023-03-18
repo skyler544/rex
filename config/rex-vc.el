c4ec70d82cb91c927413c9adf2d4e105ca680f37
Skyler Mayfield <skyler544@gmail.com>
2023-03-18 15:26:24 +0100

    steal some of doom's magit setup

--- config/rex-vc.el
+++ config/rex-vc.el
@@ -2,32 +2,85 @@
 ;;
 ;; Version control utilities.
 (use-package magit
+  :init
+  (defvar rex/magit-open-windows-in-direction 'right
+    "What direction to open new windows from the status buffer.
+For example, diffs and log buffers. Accepts `left', `right', `up', and `down'.")
+
+  (defun rex/magit-display-buffer-fn (buffer)
+    "Same as `magit-display-buffer-traditional', except...
+
+- If opened from a commit window, it will open below it.
+- Magit process windows are always opened in small windows below the current.
+- Everything else will reuse the same window."
+    (let ((buffer-mode (buffer-local-value 'major-mode buffer)))
+      (display-buffer
+       buffer (cond
+               ((and (eq buffer-mode 'magit-status-mode)
+                     (get-buffer-window buffer))
+                '(display-buffer-reuse-window))
+               ;; Any magit buffers opened from a commit window should open below
+               ;; it. Also open magit process windows below.
+               ((or (bound-and-true-p git-commit-mode)
+                    (eq buffer-mode 'magit-process-mode))
+                (let ((size (if (eq buffer-mode 'magit-process-mode)
+                                0.35
+                              0.7)))
+                  `(display-buffer-below-selected
+                    . ((window-height . ,(truncate (* (window-height) size)))))))
+
+               ;; Everything else should reuse the current window.
+               ((or (not (derived-mode-p 'magit-mode))
+                    (not (memq (with-current-buffer buffer major-mode)
+                               '(magit-process-mode
+                                 magit-revision-mode
+                                 magit-diff-mode
+                                 magit-stash-mode
+                                 magit-status-mode))))
+                '(display-buffer-same-window))
+
+               ('(rex/magit--display-buffer-in-direction))))))
+
+  (defun rex/magit--display-buffer-in-direction (buffer alist)
+    "`display-buffer-alist' handler that opens BUFFER in a direction.
+
+This differs from `display-buffer-in-direction' in one way: it will try to use a
+window that already exists in that direction. It will split otherwise."
+    (let ((direction (or (alist-get 'direction alist)
+                         rex/magit-open-windows-in-direction))
+          (origin-window (selected-window)))
+      (if-let (window (window-in-direction direction))
+          (unless magit-display-buffer-noselect
+            (select-window window))
+        (if-let (window (and (not (one-window-p))
+                             (window-in-direction
+                              (pcase direction
+                                (`right 'left)
+                                (`left 'right)
+                                ((or `up `above) 'down)
+                                ((or `down `below) 'up)))))
+            (unless magit-display-buffer-noselect
+              (select-window window))
+          (let ((window (split-window nil nil direction)))
+            (when (and (not magit-display-buffer-noselect)
+                       (memq direction '(right down below)))
+              (select-window window))
+            (display-buffer-record-window 'reuse window buffer)
+            (set-window-buffer window buffer)
+            (set-window-parameter window 'quit-restore (list 'window 'window origin-window buffer))
+            (set-window-prev-buffers window nil))))
+      (unless magit-display-buffer-noselect
+        (switch-to-buffer buffer t t)
+        (selected-window))))
   :config
   (setq magit-diff-refine-hunk t)
   (setq magit-save-repository-buffers nil)
-  (defun rex/magit-status ()
-    "Open a `magit-status' buffer and close the other window so only Magit is visible.
-If a file was visited in the buffer that was active when this
-command was called, go to its unstaged changes section."
-    (interactive)
-    (let* ((buffer-file-path (when buffer-file-name
-                               (file-relative-name buffer-file-name
-                                                   (locate-dominating-file buffer-file-name ".git"))))
-           (section-ident `((file . ,buffer-file-path) (unstaged) (status))))
-      (call-interactively #'magit-status)
-      (delete-other-windows)
-      (when buffer-file-path
-        (goto-char (point-min))
-        (cl-loop until (when (equal section-ident (magit-section-ident (magit-current-section)))
-                         (magit-section-show (magit-current-section))
-                         (recenter)
-                         t)
-                 do (condition-case nil
-                        (magit-section-forward)
-                      (error (cl-return (magit-status-goto-initial-section-1))))))))
+  (setq transient-display-buffer-action '(display-buffer-below-selected))
+  (setq magit-display-buffer-function #'rex/magit-display-buffer-fn)
+  (setq magit-bury-buffer-function #'magit-mode-quit-window)
   :general
   (rex-leader
-    "gg" 'rex/magit-status))
+    "gg" 'magit-status))
 
 ;; Show previous versions of a file
 (use-package git-timemachine
