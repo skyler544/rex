fe4e2daf3ef4751f008a44960b926b4a424fda39
Skyler Mayfield <skyler544@gmail.com>
2023-05-04 08:18:58 +0200

    update elpaca version

--- elpaca-loader.el
+++ elpaca-loader.el
@@ -1,5 +1,5 @@
 ;;; -*- lexical-binding: t -*-
-(defvar elpaca-installer-version 0.3)
+(defvar elpaca-installer-version 0.4)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
