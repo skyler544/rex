b08d6429e1f539fea4abb8cb05ba2892eb848a59
Skyler Mayfield <skyler544@gmail.com>
2023-05-05 12:52:58 +0200

    reorganize functions and evil

--- config/rex-evil.el
+++ config/rex-evil.el
@@ -14,55 +14,12 @@
   :config
   (setq evil-mode-line-format '(before . moody-mode-line-buffer-identification))
   (evil-set-initial-state 'org-agenda-mode 'normal)
+
   (evil-define-text-object +evil:whole-buffer-txtobj (count &optional _beg _end type)
     "Text object to select the whole buffer."
     (evil-range (point-min) (point-max) type))
-  (add-hook 'git-commit-mode-hook 'evil-insert-state)
-  (evil-mode)
 
-  ;; window movement; stolen from doom
-  (defun rex--window-swap (direction)
-    "Move current window to the next window in DIRECTION.
-If there are no windows there and there is only one window, split in that
-direction and place this window there. If there are no windows and this isn't
-the only window, use evil-window-move-* (e.g. `evil-window-move-far-left')."
-    (when (window-dedicated-p)
-      (user-error "Cannot swap a dedicated window"))
-    (let* ((this-window (selected-window))
-           (this-buffer (current-buffer))
-           (that-window (windmove-find-other-window direction nil this-window))
-           (that-buffer (window-buffer that-window)))
-      (when (or (minibufferp that-buffer)
-                (window-dedicated-p this-window))
-        (setq that-buffer nil that-window nil))
-      (if (not (or that-window (one-window-p t)))
-          (funcall (pcase direction
-                     ('left  #'evil-window-move-far-left)
-                     ('right #'evil-window-move-far-right)
-                     ('up    #'evil-window-move-very-top)
-                     ('down  #'evil-window-move-very-bottom)))
-        (unless that-window
-          (setq that-window
-                (split-window this-window nil
-                              (pcase direction
-                                ('up 'above)
-                                ('down 'below)
-                                (_ direction))))
-          (setq that-buffer (window-buffer that-window)))
-        (window-swap-states this-window that-window)
-        (select-window that-window))))
-  (defun rex/window-move-left ()
-    "Swap windows to the left."
-    (interactive) (rex--window-swap 'left))
-  (defun rex/window-move-down ()
-    "Swap windows downward."
-    (interactive) (rex--window-swap 'down))
-  (defun rex/window-move-up ()
-    "Swap windows upward."
-    (interactive) (rex--window-swap 'up))
-  (defun rex/window-move-right ()
-    "Swap windows to the right"
-    (interactive) (rex--window-swap 'right))
+  (evil-mode)
 
   :general
   (rex-leader
@@ -82,7 +39,8 @@ the only window, use evil-window-move-* (e.g. `evil-window-move-far-left')."
   (:keymaps 'evil-outer-text-objects-map
             "g" '+evil:whole-buffer-txtobj)
   (:keymaps 'insert
-            "C-k" 'ignore))
+            "C-k" 'ignore)
+  :hook (git-commit-mode . evil-insert-state))
 
 (use-package evil-collection
   :after evil
--- config/rex-functions.el
+++ config/rex-functions.el
@@ -4,6 +4,53 @@
 ;; packages.
 (use-package emacs :elpaca nil
   :config
+
+  ;; window movement; stolen from doom
+  (defun rex--window-swap (direction)
+    "Move current window to the next window in DIRECTION.
+If there are no windows there and there is only one window, split in that
+direction and place this window there. If there are no windows and this isn't
+the only window, use evil-window-move-* (e.g. `evil-window-move-far-left')."
+    (when (window-dedicated-p)
+      (user-error "Cannot swap a dedicated window"))
+    (let* ((this-window (selected-window))
+           (this-buffer (current-buffer))
+           (that-window (windmove-find-other-window direction nil this-window))
+           (that-buffer (window-buffer that-window)))
+      (when (or (minibufferp that-buffer)
+                (window-dedicated-p this-window))
+        (setq that-buffer nil that-window nil))
+      (if (not (or that-window (one-window-p t)))
+          (funcall (pcase direction
+                     ('left  #'evil-window-move-far-left)
+                     ('right #'evil-window-move-far-right)
+                     ('up    #'evil-window-move-very-top)
+                     ('down  #'evil-window-move-very-bottom)))
+        (unless that-window
+          (setq that-window
+                (split-window this-window nil
+                              (pcase direction
+                                ('up 'above)
+                                ('down 'below)
+                                (_ direction))))
+          (setq that-buffer (window-buffer that-window)))
+        (window-swap-states this-window that-window)
+        (select-window that-window))))
+
+  (defun rex/window-move-left ()
+    "Swap windows to the left."
+    (interactive) (rex--window-swap 'left))
+  (defun rex/window-move-down ()
+    "Swap windows downward."
+    (interactive) (rex--window-swap 'down))
+  (defun rex/window-move-up ()
+    "Swap windows upward."
+    (interactive) (rex--window-swap 'up))
+  (defun rex/window-move-right ()
+    "Swap windows to the right"
+    (interactive) (rex--window-swap 'right))
+
+  ;; override window split behavior
   (defun rex/split-and-follow-horizontally ()
     "Split window below and place point in the new window."
     (interactive)
@@ -14,6 +61,8 @@
     (interactive)
     (split-window-right)
     (other-window 1))
+
+  ;; simple way to run pieces of a buffer as shell commands
   (defun rex/shell-command-on-region-or-line ()
     "Run the command at point or in the selected region in the shell."
     (interactive)
