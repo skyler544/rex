57cfb662d102c754239276e70be0f8e96ee1aa80
Skyler Mayfield <skyler544@gmail.com>
2023-05-05 13:45:31 +0200

    formatting, organization

--- config/rex-movec.el
+++ config/rex-movec.el
@@ -1,13 +1,14 @@
 ;;; -*- lexical-binding: t -*-
 ;;
-;; This provides a vertical view of the current candidates while
-;; interacting with the minibuffer.
+;; This provides a vertical view of the current candidates while interacting
+;; with the minibuffer.
 (use-package vertico
   :elpaca (vertico :files (:defaults "extensions/*"))
   :init
   (vertico-mode)
   :custom-face
-  (vertico-group-title ((t (:inherit font-lock-comment-face))))
+  (vertico-group-title
+   ((t ( :inherit font-lock-comment-face))))
   :general
   (:keymaps 'vertico-map
             "C-j" 'vertico-next
@@ -17,12 +18,10 @@
   (setq vertico-count 12)
   (setq vertico-resize nil))
 
-;; This extends vertico with subjectively nicer keybindings for
-;; navigating directories.
-(use-package vertico-directory
-  :after vertico
-  :elpaca nil
+;; This extends vertico with keybindings for navigating directories.
+(use-package vertico-directory :elpaca nil
   :ensure nil
+  :after vertico
   :general
   (:keymaps 'vertico-map
             "RET" 'vertico-directory-enter
@@ -30,28 +29,26 @@
             "M-DEL" 'vertico-directory-delete-word)
   :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
 
-;; This completion style makes "narrowing" from a list of candidates
-;; very quick and intuitive. It also provides nice font-faces for
-;; visually distinguishing what exactly you're matching on in a
-;; string.
+;; This completion style makes "narrowing" from a list of candidates very quick
+;; and intuitive. It also provides nice font-faces for visually distinguishing
+;; what exactly you're matching on in a string.
 (use-package orderless
   :init
   (setq completion-styles '(orderless basic))
   (setq completion-category-defaults nil)
   (setq completion-category-overrides '((file) (styles partial-completion))))
 
-;; This package adds information to the minibuffer that can be used in
-;; various ways. The information can be very nice by itself, but it
-;; also adds context to certain minibuffer actions which are lacking
-;; by default, automatically making the matches more relevant. This is
-;; directly used by Embark.
+;; This package adds information to the minibuffer that can be used in various
+;; ways. The information can be very nice by itself, but it also adds context to
+;; certain minibuffer actions which are lacking by default, automatically making
+;; the matches more relevant. This is directly used by Embark.
 (use-package marginalia
   :init
   (marginalia-mode))
 
-;; A context-aware keyboard-driven equivalent of a right-click
-;; menu. This thing is a swiss-army-knife on steroids. Beware; it will
-;; make everything better than it was before.
+;; A context-aware keyboard-driven equivalent of a right-click menu. This thing
+;; is a swiss-army-knife on steroids. Beware; it will make everything better
+;; than it was before.
 (use-package embark
   :general
   ("C-;" 'embark-act)
@@ -65,23 +62,25 @@
   (setq embark-indicators '(embark-minimal-indicator))
   (setq embark-prompter 'embark-completing-read-prompter))
 
-;; A very smart package that provides lots of nice functions. Many of
-;; them are helpful for searching, but there are also commands to help
-;; with jumping between various things and interacting with the kill
-;; ring. Combine with Embark and mix well.
+;; A very smart package that provides lots of nice functions. Many of them are
+;; helpful for searching, but there are also commands to help with jumping
+;; between various things and interacting with the kill ring. Combine with
+;; Embark and mix well.
 (use-package consult
   :config
+
+  (consult-customize consult-buffer :preview-key nil)
+
   (defun rex/consult-ripgrep-in-a-directory ()
     "Query the user for a directory to grep in."
     (interactive)
     (let ((current-prefix-arg 1))
       (call-interactively 'consult-ripgrep)))
+
   (defun rex/consult-ripgrep-in-current-directory ()
     "Run consult-grep in the current directory."
     (interactive)
     (consult-ripgrep default-directory))
-
-  (consult-customize consult-buffer :preview-key nil)
 
   (defun rex/consult-buffer-state-no-tramp ()
     "Buffer state function that doesn't preview Tramp buffers."
@@ -101,7 +100,7 @@
 
   :general
   ("C-x b" 'consult-buffer)
-  (:keymaps '(normal visual)
+  (:states '(normal visual)
             "P" 'consult-yank-from-kill-ring
             "," 'consult-line)
   (:keymaps 'vertico-map
@@ -134,9 +133,8 @@
 
 (use-package embark-consult)
 
-;; Allows you to insert directories into the minibuffer prompt. It
-;; integrates with the minibuffer history, so it gets smarter the more
-;; you use it.
+;; Allows you to insert directories into the minibuffer prompt. It integrates
+;; with the minibuffer history, so it gets smarter the more you use it.
 (use-package consult-dir
   :general
   (:keymaps 'vertico-map
