5ad1ca65140c46d0725fb70dd2f063fc081a5bd5
Skyler Mayfield <skyler544@gmail.com>
2023-09-29 17:04:47 +0200

    remove dependency on helm-make by creating a custom implementation

--- config/run-command-recipes.el
+++ config/run-command-recipes.el
@@ -42,25 +42,25 @@ Automatically detects package manager based on lockfile: npm, yarn, and pnpm."
      scripts)))
 
 (defun run-command-recipe-make ()
-  "Provide commands to run Makefile targets.
-
-Requires `helm-make' (https://github.com/abo-abo/helm-make) to
-read Makefile targets, but does not require `helm' and can be
-used with any of the selectors supported by `run-command'."
-
-  (when (require 'helm-make nil t)
-    (when-let* ((project-dir
-                 (locate-dominating-file default-directory "Makefile"))
-                (makefile (concat project-dir "Makefile"))
-                (targets (helm--make-cached-targets makefile)))
-      (seq-map
-       (lambda (target)
-         (list
-          :command-name target
-          :command-line (concat "make " target)
-          :display target
-          :working-dir project-dir))
-       targets))))
+  "Provide commands to run Makefile targets."
+  (require 'make-mode)
+  (when-let* ((project-dir
+               (locate-dominating-file default-directory "Makefile"))
+              (makefile (concat project-dir "Makefile"))
+              (targets (-map #'car
+                               (with-current-buffer
+                                   (find-file-noselect makefile t)
+                                (setq makefile-need-target-pickup t)
+                                (makefile-pickup-targets)
+                                makefile-target-table))))
+              (seq-map
+               (lambda (target)
+                 (list
+                  :command-name target
+                  :command-line (concat "make " target)
+                  :display target
+                  :working-dir project-dir))
+               targets)))
 
 (provide 'run-command-recipes)
 ;;; run-command-recipes.el ends here
